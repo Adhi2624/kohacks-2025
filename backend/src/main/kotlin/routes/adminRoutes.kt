d42657c7e7ae3294a4a53f810623342f63afbbe2
Devaprakash <deva27997@gmail.com>
2025-11-01 18:32:01 +0530

    Add admin routes for facility CRUD operations
    Implemented admin routes for facility management.

--- backend/src/main/kotlin/routes/adminRoutes.kt
+++ backend/src/main/kotlin/routes/adminRoutes.kt
@@ -1,4 +1,29 @@
-package com.payir.routes
+package routes
 
-// Admin routes implementation
+import com.payir.controllers.AdminController
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
 
+fun Route.adminRoutes() {
+    route("/admin") {
+        post("/createFacility") {
+            AdminController.createFacility(call)
+        }
+
+        put("/updateFacility/{id}") {
+            AdminController.updateFacility(call)
+        }
+
+        get("/facilities") {
+            AdminController.getAllFacilities(call)
+        }
+
+        get("/facility/{id}") {
+            AdminController.getFacilityById(call)
+        }
+
+        delete("/facility/{id}") {
+            AdminController.deleteFacility(call)
+        }
+    }
+}
